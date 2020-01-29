b5d4b199bddeaaa8723aed8d59f64264a7365135
survani <giovanicode@outlook.com>
2020-01-29 00:06:43 -0800

    MVP: Completed

--- src/App.js
+++ src/App.js
@@ -1,42 +1,45 @@
-import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
-import data from './data';
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
+import data from "./data";
+
+//Context
+import ProductContext from "./contexts/ProductContext";
+import CartContext from "./contexts/CartContext";
 
 // Components
-import Navigation from './components/Navigation';
-import Products from './components/Products';
-import ShoppingCart from './components/ShoppingCart';
+import Navigation from "./components/Navigation";
+import Products from "./components/Products";
+import ShoppingCart from "./components/ShoppingCart";
 
-function App() {
-	const [products] = useState(data);
-	const [cart, setCart] = useState([]);
+const App = () => {
+  //states
+  const [products] = useState(data);
+  const [cart, setCart] = useState([]);
 
-	const addItem = item => {
-		// add the given item to the cart
-	};
+  //adds item to cart
+  const addItem = item => {
+    //item = the specific item when it is added to cart...
+    console.log("gg: Check item: ", item);
 
-	return (
-		<div className="App">
-			<Navigation cart={cart} />
+    // add the given item to the cart
+    setCart([...cart, item]);
+  };
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+  return (
+    <div className="App">
+      {/* Context.Provider used here to remove prop drilling */}
+      <ProductContext.Provider value={{ products, addItem }}>
+        <CartContext.Provider value={{ cart }}>
+          <Navigation cart={cart} />
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
-	);
-}
+          {/* ROUTES BELOW: Using Context Api now */}
+          <Route exact path="/" component={Products} />
+          <Route path="/cart" component={ShoppingCart} />
+          {/* ROUTES ABOVE: Using Context Api now */}
+        </CartContext.Provider>
+      </ProductContext.Provider>
+    </div>
+  );
+};
 
 export default App;
